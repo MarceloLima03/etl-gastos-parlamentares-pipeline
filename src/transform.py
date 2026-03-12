a4cf8a80b9a4ea8bfc9cec1c53e0d00d535d7bcc
MarceloLima03 <marcelo_denis@outlook.com>
2026-03-12 14:19:17 -0300

    feat: adiciona função transformar_dados_deputado

--- src/transform.py
+++ src/transform.py
@@ -1,13 +1,36 @@
 import pandas as pd
-from extract import buscar_gastos
+from extract import buscar_deputados, buscar_gastos
 from utils import ID_PARLAMENTAR, PERIODO
 
-def transformar_gastos(dados):
+def transformar_dados_deputado(response):
     """
     Transforma os dados em DataFrame
 
     Args:
-        dados (list): Lista com todos os dados de despesas do deputado
+        response (response): Lista com todos os dados de despesas do deputado
+    
+    Returns:
+        pd.DataFrame: DataFrame com os dados transformados
+    """
+    dados = response.json()['dados']
+    dados_desejados = {
+        'id': dados['id'],
+        'nome': dados['nomeCivil'],
+        'partido': dados['ultimoStatus']['siglaPartido'],
+        'estado': dados['ultimoStatus']['siglaUf'],
+        'email': dados['ultimoStatus']['gabinete']['email'],
+        'nascimento': dados['dataNascimento'],
+        'escolaridade': dados['escolaridade']
+    }
+    data_frame = pd.DataFrame([dados_desejados])
+    return data_frame
+
+def transformar_gastos(response):
+    """
+    Transforma os dados em DataFrame
+
+    Args:
+        response (response): Lista com todos os dados de despesas do deputado
     
     Returns:
         pd.DataFrame: DataFrame com os dados transformados
@@ -15,11 +38,8 @@ def transformar_gastos(dados):
     Notes:
          datas inválidas são substituídas por 1990-01-01
     """
-    data_frame = pd.DataFrame(dados)
+    data_frame = pd.DataFrame(response)
     data_frame.fillna({'urlDocumento': ''}, inplace=True)
     data_frame['dataDocumento'] = pd.to_datetime(data_frame['dataDocumento'], errors='coerce', dayfirst=True) 
     data_frame['dataDocumento'] = data_frame['dataDocumento'].fillna(pd.Timestamp('1990-01-01'))
     return data_frame
-
-response = buscar_gastos(ID_PARLAMENTAR, PERIODO)
-data_frame = transformar_gastos(response)
