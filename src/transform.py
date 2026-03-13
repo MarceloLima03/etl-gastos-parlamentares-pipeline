729385cf4ec63bbd20da8bc901512c829a81c16b
MarceloLima03 <marcelo_denis@outlook.com>
2026-03-12 21:06:08 -0300

    feat: finaliza transform.py com limpeza de dados

--- src/transform.py
+++ src/transform.py
@@ -1,17 +1,16 @@
 import pandas as pd
-from extract import buscar_deputados, buscar_gastos
-from utils import ID_PARLAMENTAR, PERIODO
 
 def transformar_dados_deputado(response):
     """
     Transforma os dados em DataFrame
 
     Args:
-        response (response): Lista com todos os dados de despesas do deputado
+        response (Response): Lista com todos os dados de despesas do deputado
     
     Returns:
         pd.DataFrame: DataFrame com os dados transformados
     """
+    print("Aguarde: Normalizando os dados do deputado...")
     dados = response.json()['dados']
     dados_desejados = {
         'id': dados['id'],
@@ -30,7 +29,7 @@ def transformar_gastos(response):
     Transforma os dados em DataFrame
 
     Args:
-        response (response): Lista com todos os dados de despesas do deputado
+        response (list): Lista com todos os dados de despesas do deputado
     
     Returns:
         pd.DataFrame: DataFrame com os dados transformados
@@ -38,6 +37,7 @@ def transformar_gastos(response):
     Notes:
          datas inválidas são substituídas por 1990-01-01
     """
+    print("Aguarde: Normalizando os gastos do deputado...")
     data_frame = pd.DataFrame(response)
     data_frame.fillna({'urlDocumento': ''}, inplace=True)
     data_frame['dataDocumento'] = pd.to_datetime(data_frame['dataDocumento'], errors='coerce', dayfirst=True) 
