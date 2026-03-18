2795b8a779eaa50e9d3dc6cc8b88e770737eee1f
MarceloLima03 <marcelo_denis@outlook.com>
2026-03-18 12:36:08 -0300

    refactor: encapsula pipeline na função executar_pipeline

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ from transform import transformar_dados_deputado, transformar_gastos
 from load import conexao, criar_tabelas, inserir_deputado, inserir_gastos
 from utils import ID_PARLAMENTAR, PERIODO
 
-conn = conexao()
-try:
-    criar_tabelas(conn)
+def executar_pipeline():
+    conn = conexao()
+    try:
+        criar_tabelas(conn)
 
-    response = buscar_deputados(ID_PARLAMENTAR)
-    data_frame_deputados = transformar_dados_deputado(response)
-    inserir_deputado(conn, data_frame_deputados)
+        response = buscar_deputados(ID_PARLAMENTAR)
+        data_frame_deputados = transformar_dados_deputado(response)
+        inserir_deputado(conn, data_frame_deputados)
 
-    response = buscar_gastos(ID_PARLAMENTAR, PERIODO)
-    data_frame_gastos = transformar_gastos(response)
-    id_deputado = int(data_frame_deputados['id'].iloc[0])
-    inserir_gastos(conn, data_frame_gastos, id_deputado)
-finally:
-    if conn is not None:
-        try:
-            conn.close()
-        except pyodbc.Error as e:
-            print(f'Erro ao fechar cursor: {e}')
+        response = buscar_gastos(ID_PARLAMENTAR, PERIODO)
+        data_frame_gastos = transformar_gastos(response)
+        id_deputado = int(data_frame_deputados['id'].iloc[0])
+        inserir_gastos(conn, data_frame_gastos, id_deputado)
+    finally:
+        if conn is not None:
+            try:
+                conn.close()
+                print('Aguarde: Fechando conexão com o banco...')
+            except pyodbc.Error as e:
+                print(f'Erro ao fechar conexão: {e}')
